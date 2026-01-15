0044628635ee20e321d259c8492c8d35f9bfb897
David Lai <laidav@gmail.com>
2026-01-15 20:36:24 +0000

    set up route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,9 +3,11 @@
 use App\Http\Controller\API\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return [];
+// AUTH ROUTES
+
+Route::prefix('v1')->group(function() {
+    Route::post('/register', [AuthController::class, 'register']);
 });
 
-Route::get('/reset-password/{token}', [AuthController::class, 'showResetForm'])
-    ->name('password.reset');
+// Route::get('/reset-password/{token}', [AuthController::class, 'showResetForm'])
+//     ->name('password.reset');
