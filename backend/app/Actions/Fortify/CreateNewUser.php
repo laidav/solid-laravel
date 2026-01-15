1dd4b89d6b77d52df4ddbbd40b6a17ec6fe05561
David Lai <laidav@gmail.com>
2026-01-15 20:51:14 +0000

    add logging

--- backend/app/Actions/Fortify/CreateNewUser.php
+++ backend/app/Actions/Fortify/CreateNewUser.php
@@ -19,6 +19,7 @@ class CreateNewUser implements CreatesNewUsers
      */
     public function create(array $input): User
     {
+        logger($input);
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => [
