1a4c1cbc285e6426c9f1b33e9e0130c3e29f43d6
David Lai <laidav@gmail.com>
2026-01-15 12:36:23 +0000

    install ide helper

--- backend/app/Http/API/AuthController.php
+++ backend/app/Http/API/AuthController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controller\API;
 
+use App\Actions\Fortify\CreateNewUser;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+
 class AuthController extends APIController {
   public function showResetForm() {
 
     
+  }
+
+  public function register(Request $request, CreateNewUser $creator): JsonResponse {
+        // Create the user using Fortify's action
+        $user = $creator->create($request->only([
+            'name',
+            'email',
+            'password',
+            'password_confirmation',
+        ]));
+
+        return response()->json(['userId' => $user->id], Response::HTTP_CREATED);
   }
 }
