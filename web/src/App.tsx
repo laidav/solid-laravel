bf355d7110cab3da2660ef181540ecf82bf2e865
David Lai <laidav@gmail.com>
2026-01-15 23:29:49 +0000

    install routing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,35 +1,14 @@
-import solidLogo from "./assets/solid.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { createReactable } from "./Helpers/createReactable";
-import { RxCounter } from "./RxCounter";
+import { Route, Router } from "@solidjs/router";
 
 function App() {
-  const [count, { increment }] = createReactable(() => RxCounter());
-
-  console.log("function");
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={increment}>count is {count()}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
-    </>
+    <div>
+      <Router>
+        <Route path="/" component={() => <h1>Home</h1>} />
+        <Route path="/register" component={() => <h1>Register</h1>} />
+      </Router>
+    </div>
   );
 }
 
