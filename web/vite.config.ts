136aaaf0b75a5adaeaabd1e6c5e992f6bb2a80cf
David Lai <laidav@gmail.com>
2026-02-03 23:03:35 +0000

    restore polling

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,4 +3,12 @@ import solid from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [solid()],
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 1000,
+    },
+    host: true,
+    port: 5173,
+  },
 });
